e5d999df407411b5a61d2fc445643b6b8fda87a9
VedEti123 <30673010+VedEti123@users.noreply.github.com>
2022-11-11 23:59:20 -0600

    Data Set

---
+++ src/Webscraper.h
@@ -0,0 +1,14 @@
+//
+// Created by vedet on 11/11/2022.
+//
+
+#ifndef CS225_FINAL_PROJECT_WEBSCRAPER_H
+#define CS225_FINAL_PROJECT_WEBSCRAPER_H
+
+
+class Webscraper {
+
+};
+
+
+#endif //CS225_FINAL_PROJECT_WEBSCRAPER_H
