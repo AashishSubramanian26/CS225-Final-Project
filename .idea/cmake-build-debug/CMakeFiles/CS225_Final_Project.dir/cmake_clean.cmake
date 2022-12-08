59986bebc747457615ab0c081125fb87474d0c1f
Adipat2003 <99374679+Adipat2003@users.noreply.github.com>
2022-12-08 17:09:58 -0600

    final

---
+++ .idea/cmake-build-debug/CMakeFiles/CS225_Final_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS225_Final_Project.dir/src/C++/main.cpp.o"
+  "CMakeFiles/CS225_Final_Project.dir/src/C++/main.cpp.o.d"
+  "CMakeFiles/CS225_Final_Project.dir/src/Tests/TestingData.cpp.o"
+  "CMakeFiles/CS225_Final_Project.dir/src/Tests/TestingData.cpp.o.d"
+  "CS225_Final_Project"
+  "CS225_Final_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS225_Final_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
