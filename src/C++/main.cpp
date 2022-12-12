1cef17b3e5b6888830b0c4848e7faeda8636d120
Adipat2003 <99374679+Adipat2003@users.noreply.github.com>
2022-12-12 15:35:25 -0600

    fin

--- src/C++/main.cpp
+++ src/C++/main.cpp
@@ -4,14 +4,22 @@ using namespace std;
 #include <cstdio>
 #include <vector>
 
-int main()
-{
+int main() {
     Graph temp = Graph();
-    temp.parsing2();
-
-    vector<string> temp89 = temp.shortestPath(temp.getMap(), "Ladder", "Honey");
+    temp.parsing1();
+    vector<string> temp89 = temp.shortestPath(temp.getMap(), "Paris", "Candy");
     map<string, vector<pair<string, unsigned>>> temp88 = temp.getMap();
-    //vector<vector<string>> temp2 = temp.yens(temp.getMap(), "Ladder", "Honey", 10);
-
+    cout<<"Djikstra's out ->"<<endl;
+    for (unsigned i = 0; i < temp89.size(); i++) {
+        cout << temp89[i];
+    }
+    cout << "\n" << "Yen's output ->" << endl;
+    vector<vector<string>> temp2 = temp.yens(temp.getMap(), "Paris", "Candy", 5);
+    for (unsigned i = 0; i < temp2.size(); i++) {
+        for (unsigned j = 0; j < temp2[i].size(); j++) {
+            cout << temp2.at(i).at(j);
+        }
+        cout << "\n";
+    }
     return 1;
 }
