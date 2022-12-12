2ddc7a6a3fa90495a5de7875ab3ea1b00b5bc327
AashishSubramanian26 <aashish.subramanian26@gmail.com>
2022-12-12 16:16:28 -0600

    Finish

---
+++ build/src/C++/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/CS225-Final-Project/src/C++/Graph.cpp" "src/C++/CMakeFiles/src.dir/Graph.cpp.o" "gcc" "src/C++/CMakeFiles/src.dir/Graph.cpp.o.d"
+  "/workspaces/CS225-Final-Project/src/C++/Webscraper.cpp" "src/C++/CMakeFiles/src.dir/Webscraper.cpp.o" "gcc" "src/C++/CMakeFiles/src.dir/Webscraper.cpp.o.d"
+  "/workspaces/CS225-Final-Project/src/C++/main.cpp" "src/C++/CMakeFiles/src.dir/main.cpp.o" "gcc" "src/C++/CMakeFiles/src.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/C++/main.cpp
+++ src/C++/main.cpp
@@ -6,20 +6,25 @@ using namespace std;
 
 int main() {
     Graph temp = Graph();
+
+    // Choose between parsing 1 and parsing two by commenting and uncommenting the following code
+    // Parsing two will go through a mush bigger dataset and will take longer to run, while parsing 2 is shorter and takes less time
     temp.parsing1();
+    //temp.parsing2();
+
     vector<string> temp89 = temp.shortestPath(temp.getMap(), "Paris", "Candy");
     map<string, vector<pair<string, unsigned>>> temp88 = temp.getMap();
-    cout<<"Djikstra's out ->"<<endl;
+    cout<<"Djikstra's out -> ";
     for (unsigned i = 0; i < temp89.size(); i++) {
-        cout << temp89[i];
+        cout << temp89[i]<< ", ";
     }
-    cout << "\n" << "Yen's output ->" << endl;
+    cout << "\n\n" << "Yen's output -> " << endl;
     vector<vector<string>> temp2 = temp.yens(temp.getMap(), "Paris", "Candy", 5);
     for (unsigned i = 0; i < temp2.size(); i++) {
         for (unsigned j = 0; j < temp2[i].size(); j++) {
-            cout << temp2.at(i).at(j);
+            cout << temp2.at(i).at(j) << ", ";
         }
-        cout << "\n";
+        cout << "\n____________________________________________\n";
     }
     return 1;
 }
